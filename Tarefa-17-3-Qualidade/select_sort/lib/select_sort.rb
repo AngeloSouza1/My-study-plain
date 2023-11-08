fbea470ca397e93e3fca16cade473fd16ee30be6
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-08 18:26:54 -0300

    refactor: update code

--- Tarefa-17-3-Qualidade/select_sort/lib/select_sort.rb
+++ Tarefa-17-3-Qualidade/select_sort/lib/select_sort.rb
@@ -2,7 +2,34 @@
 
 require_relative "select_sort/version"
 
+
 module SelectSort
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.selection_sort(arr)
+    n = arr.length
+
+    for i in 0...n-1
+      min_idx = i
+      for j in (i + 1)...n
+        min_idx = j if arr[j] < arr[min_idx]
+      end
+
+      if min_idx != i
+        swap(arr, min_idx, i)
+      end
+    end
+
+    arr # Retorna o array ordenado
+  end
+
+  def self.swap(arr, x, y)
+    temp = arr[x]
+    arr[x] = arr[y]
+    arr[y] = temp
+  end
 end
+
+
+    
+
