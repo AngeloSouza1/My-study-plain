e9d703c7614319fdd2dba06d0cc1fc43c3d46ac5
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-07 21:48:11 -0300

    refactor: update code

--- Tarefa-17-3-Qualidade/find_missing/lib/find_missing.rb
+++ Tarefa-17-3-Qualidade/find_missing/lib/find_missing.rb
@@ -1,31 +1,30 @@
-
-
 require_relative "find_missing/version"
 
 module FindMissing
   class Error < StandardError; end
 
-  def find_missing(arr)
+  def self.find_missing(arr)
     n = arr.length
+
+    # Verificar se o array está vazio
+    return nil if n == 0
+
     temp = Array.new(n + 1, 0)
-  
+
     for i in 0...n
       temp[arr[i] - 1] = 1
     end
-  
+
     ans = nil
     for i in 0..n
       if temp[i] == 0
         ans = i + 1
+        break
       end
     end
-  
+
     return ans
   end
-
-
-
-
-
-
 end
+
+
