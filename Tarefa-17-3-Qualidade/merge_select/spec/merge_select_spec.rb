d3613a1cc42c4902f253849d466a234eb2756596
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-08 19:47:15 -0300

    tests: change more 4 tests

--- Tarefa-17-3-Qualidade/merge_select/spec/merge_select_spec.rb
+++ Tarefa-17-3-Qualidade/merge_select/spec/merge_select_spec.rb
@@ -5,16 +5,43 @@ require_relative '../lib/merge_select'
 RSpec.describe MergeSelect do
   describe 'Tests MergeSelect' do
     let(:arr) { [12, 11, 13, 5, 6, 7] }
-    let(:sorted_arr) { arr.sort }
 
     it 'correctly merges two subarrays' do
       l = 0
       m = 2
       r = 5
-      expected_result = sorted_arr[0..5]
-      MergeSort.merge(arr, l, m, r)
+      expected_result = [5, 6, 7, 11, 12, 13]
+      MergeSelect.mergeSort(arr, l, r)
       expect(arr).to eq(expected_result)
     end
+  
+    it 'correctly sorts a single-element array' do
+      single_element_arr = [1]
+      expected_result = [1]
+      MergeSelect.mergeSort(single_element_arr, 0, 0)
+      expect(single_element_arr).to eq(expected_result)
+    end
+    it 'correctly handles an empty array' do
+      empty_arr = []
+      expected_result = []
+      MergeSelect.mergeSort(empty_arr, 0, -1)
+      expect(empty_arr).to eq(expected_result)
+    end
+
+    it 'correctly handles an already sorted array' do
+      sorted_arr = [1, 2, 3, 4, 5]
+      expected_result = [1, 2, 3, 4, 5]
+      MergeSelect.mergeSort(sorted_arr, 0, 4)
+      expect(sorted_arr).to eq(expected_result)
+    end
+
+    it 'correctly handles a reverse-sorted array' do
+      reverse_sorted_arr = [5, 4, 3, 2, 1]
+      expected_result = [1, 2, 3, 4, 5]
+      MergeSelect.mergeSort(reverse_sorted_arr, 0, 4)
+      expect(reverse_sorted_arr).to eq(expected_result)
+    end
+  
+  
   end
 end
-
