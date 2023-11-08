c53ea6dca66207028fcf658124ed464f9022625b
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-08 15:55:30 -0300

    teste: update code

--- Tarefa-17-3-Qualidade/find_missing/spec/find_missing_spec.rb
+++ Tarefa-17-3-Qualidade/find_missing/spec/find_missing_spec.rb
@@ -1,35 +1,24 @@
 require "find_missing"
 
 RSpec.describe FindMissing do
-    it 'encontra o número ausente quando o número ausente é 4' do
-      arr = [1, 2, 3, 5]
-      find = Object.new.extend(FindMissing)
-        expect(find.find_missing(arr)).to eq(4)
-    end
+  it 'encontra o número ausente quando o número ausente é 4' do
+    arr = [1, 2, 3, 5]
+    expect(FindMissing.find_missing(arr)).to eq(4)
+  end
 
-   it 'encontra o número ausente quando o número ausente é 1' do
+  it 'encontra o número ausente quando o número ausente é 1' do
     arr = [2, 3, 4, 5]
-    find = Object.new.extend(FindMissing)
-    expect(find.find_missing(arr)).to eq(1)
-   end
-    
-    
-   it 'encontra o número ausente quando o número ausente é 7' do
-      arr = [1, 2, 3, 4, 5, 6]
-      find = Object.new.extend(FindMissing)
-      expect(find.find_missing(arr)).to eq(7)
-   end
+    expect(FindMissing.find_missing(arr)).to eq(1)
+  end
 
-    it 'lida com um array vazio' do
-      arr = []
-      find = Object.new.extend(FindMissing)
-      expect(find.find_missing(arr)).to eq(nil)
-     end
+  it 'encontra o número ausente quando o número ausente é 7' do
+    arr = [1, 2, 3, 4, 5, 6]
+    expect(FindMissing.find_missing(arr)).to eq(7)
+  end
 
-    #  it 'lida com um array sem número ausente' do
-    #    arr = [1, 2, 3, 4, 5]
-    #    find = Object.new.extend(FindMissing)
-    #    expect(find.find_missing(arr)).to eq(nil)
-    #  end
-  
-end
+  it 'lida com um array vazio' do
+    arr = []
+    expect(FindMissing.find_missing(arr)).to eq(nil)
+  end
+
+  end
