fb5813ac02d409655b7197bfad08429a177a3ce3
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-08 16:06:12 -0300

    feat: update code

---
+++ Tarefa-17-3-Qualidade/select_sort/lib/select_sort.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "select_sort/version"
+
+module SelectSort
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ Tarefa-17-3-Qualidade/select_sort/lib/select_sort/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SelectSort
+  VERSION = "0.1.0"
+end
