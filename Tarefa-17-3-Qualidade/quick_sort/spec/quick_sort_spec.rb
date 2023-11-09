d41f8d7b8696a89c85efe3e77462a28355f95f7a
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-09 19:06:01 -0300

    tests: change more 5 tests

--- Tarefa-17-3-Qualidade/quick_sort/spec/quick_sort_spec.rb
+++ Tarefa-17-3-Qualidade/quick_sort/spec/quick_sort_spec.rb
@@ -10,7 +10,65 @@ RSpec.describe "QuickSort" do
       QuickSort.quick_sort(arr, 0, n-1)
       expect(arr).to eq(expected_result) 
       end
-  end
+  
+      it "sorts an array with elements in ascending order" do
+        arr = [1, 2, 3, 4, 5]
+        n = arr.length
+        expected_result = [1, 2, 3, 4, 5]
+        QuickSort.quick_sort(arr, 0, n-1)
+        expect(arr).to eq(expected_result) 
+      end
+    
+      it "sorts an array with elements in descending order" do
+        arr = [5, 4, 3, 2, 1]
+        n = arr.length
+        expected_result = [1, 2, 3, 4, 5]
+        QuickSort.quick_sort(arr, 0, n-1)
+        expect(arr).to eq(expected_result) 
+      end
+        
+      it "sorts an array with repeated elements" do
+        arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+        n = arr.length
+        expected_result = [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+        QuickSort.quick_sort(arr, 0, n-1)
+        expect(arr).to eq(expected_result) 
+      end
+  
+      it "sorts an array with a single element" do
+        arr = [42]
+        n = arr.length
+        expected_result = [42]
+        QuickSort.quick_sort(arr, 0, n-1)
+        expect(arr).to eq(expected_result) 
+      end
+  
+    
+      it "sorts an empty array" do
+        arr = []
+        n = arr.length
+        expected_result = []
+        QuickSort.quick_sort(arr, 0, n-1)
+        expect(arr).to eq(expected_result) 
+      end
+
+   
+    
+  
+  
+  
+  
+
+  
+  
+    end
 end
+
+
+
+
+
+
+
 
 
