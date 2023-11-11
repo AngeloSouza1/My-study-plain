c76d0c04b42841301e8112db56e488569ab534d2
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-11 20:53:01 -0300

    feat: create code

---
+++ Tarefa-17-3-Qualidade/minimize-cash-flow/lib/minimize/cash/flow.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "flow/version"
+
+module Minimize
+  module Cash
+    module Flow
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ Tarefa-17-3-Qualidade/minimize-cash-flow/spec/minimize/cash/flow_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Minimize::Cash::Flow do
+  it "has a version number" do
+    expect(Minimize::Cash::Flow::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
