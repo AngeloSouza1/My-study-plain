e271d56f58d7bb939ae9b5a470ecd8e73f3b2a50
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 11:48:25 -0300

    chore: update code

--- Tarefa-17-3-Qualidade/sum_minute/lib/sum_minute.rb
+++ Tarefa-17-3-Qualidade/sum_minute/lib/sum_minute.rb
@@ -4,5 +4,11 @@ require_relative "sum_minute/version"
 
 module SumMinute
   class Error < StandardError; end
-  # Your code goes here...
+ 
+  def self.min_sum(arr)
+    min_val = arr.min
+    return min_val * (arr.length - 1)
+  end
+  
+ #Em Ruby, usamos o método min para encontrar o valor mínimo no array, em vez de min_element.
 end
