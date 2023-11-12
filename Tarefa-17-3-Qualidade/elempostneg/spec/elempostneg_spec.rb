22c175c69a422c70353d45d5742b2a4c59c455ac
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 12:12:25 -0300

    test: change tests

--- Tarefa-17-3-Qualidade/elempostneg/spec/elempostneg_spec.rb
+++ Tarefa-17-3-Qualidade/elempostneg/spec/elempostneg_spec.rb
@@ -1,11 +1,30 @@
-# frozen_string_literal: true
 
-RSpec.describe Elempostneg do
-  it "has a version number" do
-    expect(Elempostneg::VERSION).not_to be nil
-  end
+require_relative '../lib/elempostneg' 
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+RSpec.describe 'reorganiza elementos positivos em locais pares e elementos negativos em locais ímpares' do
+      it ' Array misto com positivos e negativos' do
+        arr = [1, -2, 3, -4, 5]
+        expect(Elempostneg.rearrange_array(arr)).to eq([1, -2, 3, -4, 5])
+      end
+      
+      it 'Array com apenas números negativos' do
+        arr = [-1, -3, -5, -7]
+        expect(Elempostneg.rearrange_array(arr)).to eq([-1, -3, -5, -7])
+      end
+      
+      it 'Array vazio' do
+        arr = []
+        expect(Elempostneg.rearrange_array(arr)).to eq([])
+      end  
+       
+      it 'Array com zeros' do
+        arr = [0, 0, 0, 0]
+        expect(Elempostneg.rearrange_array(arr)).to eq([0, 0, 0, 0])
+      end 
+      
+      it 'Array com um único elemento' do
+        arr = [42]
+        expect(Elempostneg.rearrange_array(arr)).to eq([42])
+      end
+    
 end
