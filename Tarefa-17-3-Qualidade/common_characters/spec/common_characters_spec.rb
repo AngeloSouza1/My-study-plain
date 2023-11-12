e378fd7893951ff19398012c13c11b63a64b425a
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 10:35:38 -0300

    test: change tests

--- Tarefa-17-3-Qualidade/common_characters/spec/common_characters_spec.rb
+++ Tarefa-17-3-Qualidade/common_characters/spec/common_characters_spec.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
+require_relative '../lib/common_characters'
 
-RSpec.describe CommonCharacters do
-  it "has a version number" do
-    expect(CommonCharacters::VERSION).not_to be nil
+RSpec.describe '#caracteres_comuns' do
+  it 'retorna caracteres comuns em várias strings em ordem alfabética' do
+    strings = ["geeksforgeeks", "gemkstones", "acknowledges", "aguelikes"]
+    resultado = CommonCharacters.caracteres_comuns(strings, :alfabetica)
+    resultado_esperado = "e, g, k, s"
+    expect(resultado).to eq(resultado_esperado)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'retorna caracteres comuns em várias strings em ordem lexicográfica' do
+    strings = ["geeksforgeeks", "gemkstones", "acknowledges", "aguelikes"]
+    resultado = CommonCharacters.caracteres_comuns(strings, :lexicografica)
+    resultado_esperado = "e, g, k, s"
+    expect(resultado).to eq(resultado_esperado)
+  end
+  
+  it 'retorna uma string vazia quando a matriz de strings está vazia' do
+    strings = []
+    resultado = CommonCharacters.caracteres_comuns(strings)
+    resultado_esperado = []
+    expect(resultado).to eq(resultado_esperado)
+  end
+
+  it 'retorna uma string vazia quando não há caracteres comuns' do
+    strings = ["abc", "def", "xyz"]
+    resultado = CommonCharacters.caracteres_comuns(strings)
+    expect(resultado).to eq('')
   end
 end
