ce2139b45f240dc0e8d6583b5635b0aaa8359cd5
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 11:05:38 -0300

    test: change tests

--- Tarefa-17-3-Qualidade/day_weak/spec/day_weak_spec.rb
+++ Tarefa-17-3-Qualidade/day_weak/spec/day_weak_spec.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
-RSpec.describe DayWeak do
-  it "has a version number" do
-    expect(DayWeak::VERSION).not_to be nil
-  end
+require_relative '../lib/day_weak.rb'
 
-  it "does something useful" do
-    expect(false).to eq(true)
+# Descrever os testes para a função day_of_the_week
+RSpec.describe 'day_of_the_week' do
+  it 'calcula o dia da semana corretamente' do
+    # Teste para uma data conhecida
+    expect(DayWeak.day_of_the_week(2023, 11, 12)).to eq(0) 
+   end
+end
+
+# Descrever os testes para a função day_name
+RSpec.describe 'day_name' do
+  it 'retorna o nome do dia corretamente' do
+    # Teste para um número de dia conhecido
+    expect(DayWeak.day_name(1)).to eq('Segunda-feira')
+  
   end
 end
+
