9b25da11d6fd39baedb6bfd13c7dc1b6b8709a87
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 11:34:42 -0300

    test: change tests

--- Tarefa-17-3-Qualidade/reoder_index/spec/reoder_index_spec.rb
+++ Tarefa-17-3-Qualidade/reoder_index/spec/reoder_index_spec.rb
@@ -1,11 +1,19 @@
-# frozen_string_literal: true
-
-RSpec.describe ReoderIndex do
-  it "has a version number" do
-    expect(ReoderIndex::VERSION).not_to be nil
+require_relative '../lib/reoder_index.rb'
+RSpec.describe '#reordenar_arrays' do
+  it 'reordena o array de acordo com os índices fornecidos' do
+    arr = [10, 11, 12]
+    index = [1, 0, 2]
+    ReoderIndex.reordenar_arrays(arr, index)
+    expect(arr).to eq([11, 10, 12])
+    expect(index).to eq([1, 0, 2])
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'reordena outro array de acordo com os índices fornecidos 2 ' do
+    arr = [50, 40, 70, 60, 90]
+    index = [3, 0, 4, 1, 2]
+    ReoderIndex.reordenar_arrays(arr, index)
+    expect(arr).to eq([60, 50, 90, 40, 70])
+    expect(index).to eq([3, 0, 4, 1, 2])
   end
 end
+
