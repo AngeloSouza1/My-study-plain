8a9982459aa36e02e5b8735af87ee41cfe73d15c
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 11:33:31 -0300

    chore: update code

--- Tarefa-17-3-Qualidade/reoder_index/lib/reoder_index.rb
+++ Tarefa-17-3-Qualidade/reoder_index/lib/reoder_index.rb
@@ -4,5 +4,12 @@ require_relative "reoder_index/version"
 
 module ReoderIndex
   class Error < StandardError; end
-  # Your code goes here...
+    def self.reordenar_arrays(arr, index)
+      # Crie uma cópia do array original para evitar a modificação direta dele
+      temp_arr = arr.dup
+    
+      # Reordene o array 'temp_arr' de acordo com os índices fornecidos
+      arr.clear
+      index.each { |idx| arr << temp_arr[idx] }
+    end
 end
