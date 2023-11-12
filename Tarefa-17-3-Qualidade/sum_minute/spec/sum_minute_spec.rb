d4a945ab14b12700a121503b4c0369cbf81bf732
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 11:48:56 -0300

    test: change tests

--- Tarefa-17-3-Qualidade/sum_minute/spec/sum_minute_spec.rb
+++ Tarefa-17-3-Qualidade/sum_minute/spec/sum_minute_spec.rb
@@ -1,11 +1,24 @@
-# frozen_string_literal: true
 
-RSpec.describe SumMinute do
-  it "has a version number" do
-    expect(SumMinute::VERSION).not_to be nil
+require_relative '../lib/sum_minute'
+
+RSpec.describe 'min_sum' do
+  it 'calcula o valor mínimo corretamente' do
+    arr = [3, 6, 2, 8, 7, 5]
+    expected_result = 10 # O valor mínimo é 2, e o resultado esperado é 2 * (6 - 1) = 10
+
+    result = SumMinute.min_sum(arr)
+
+    expect(result).to eq(expected_result)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+ 
+
+  it 'calcula o valor mínimo corretamente com um único elemento' do
+    arr = [5]
+    expected_result = 0 # Com apenas um elemento, o resultado esperado é sempre 0
+
+    result = SumMinute.min_sum(arr)
+
+    expect(result).to eq(expected_result)
   end
 end
