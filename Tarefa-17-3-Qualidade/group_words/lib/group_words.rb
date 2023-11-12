e15b7c1f7c98155c0f8b7565398f95c12ee95b2e
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-11-12 10:47:11 -0300

    chore: update code

--- Tarefa-17-3-Qualidade/group_words/lib/group_words.rb
+++ Tarefa-17-3-Qualidade/group_words/lib/group_words.rb
@@ -4,5 +4,25 @@ require_relative "group_words/version"
 
 module GroupWords
   class Error < StandardError; end
-  # Your code goes here...
+  
+  def self.agrupa_palavras_com_mesmo_conjunto_caracteres(words)
+    conjunto_palavras = Hash.new { |hash, key| hash[key] = [] }
+  
+    words.each do |word|
+      conjunto = word.chars.sort.join
+      conjunto_palavras[conjunto] << word
+    end
+  
+    conjunto_palavras.each_value do |palavras|
+      puts palavras.join(", ")
+    end
+  end
+  
+  # Exemplo de uso
+  words = ["may", "student", "students", "dog",
+           "studentssess", "god", "cat", "act",
+           "tab", "bat", "flow", "wolf", "lambs",
+           "amy", "yam", "balms", "looped", 
+           "poodle"]
+  
 end
